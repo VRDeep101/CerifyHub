be7738ab1fac163c232772c55ee23cb9f70eb9cb
Deep Lambhade <deeplambhade101@gmail.com>
2026-02-15 20:58:53 +0530

    Add structure

---
+++ data.js
@@ -0,0 +1,14 @@
+const certificates = [
+  {
+    title: "AI Workshop",
+    description: "Learned ML basics and practical AI applications.",
+    date: "Aug 2025",
+    image: "images/ai.jpg"
+  },
+  {
+    title: "Frontend Bootcamp",
+    description: "React & UI development workshop.",
+    date: "Sept 2025",
+    image: "images/frontend.jpg"
+  }
+];
