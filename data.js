e7a567bfee753687718480a36095c92abc6ba4a6
Deep Lambhade <deeplambhade101@gmail.com>
2026-02-16 00:55:08 +0530

    Fix formatting issues in certifications array

--- data.js
+++ data.js
@@ -1,15 +1,13 @@
 const certifications = [
-{
-  title: "Be10x AI Masterclass",
-  description: "Completed the Be10X AI Workshop, where I learned 15+ AI tools focused on productivity, automation, and workflow optimization.",
-  image: "images/be10X.jpg",   // thumbnail image
-  pdf: "images/be10X.pdf"
-}, 
+  {
+    title: "Be10x AI Masterclass",
+    description: "Completed the Be10X AI Workshop, where I learned 15+ AI tools focused on productivity, automation, and workflow optimization.",
+    image: "images/be10X.jpg",
+    pdf: "images/be10X.pdf"
+  },
   {
     title: "Cyber Security Webinar",
     description: "Ethical hacking fundamentals.",
     image: "images/certificate2.png"
   }
 ];
-
-
