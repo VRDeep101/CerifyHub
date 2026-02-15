10dadb3b25c383ab690482cd1d6c2f308afc3c31
Deep Lambhade <deeplambhade101@gmail.com>
2026-02-16 01:07:40 +0530

    Normalise

--- data.js
+++ data.js
@@ -1,13 +1,14 @@
 const certifications = [
   {
     title: "Be10x AI Masterclass",
-    description: "Completed the Be10X AI Workshop, where I learned 15+ AI tools focused on productivity, automation, and workflow optimization.",
+    description: "Completed the Be10X AI Workshop, learned 15+ AI tools focused on productivity, automation, and workflow optimization.",
     image: "images/be10X.jpg",
     pdf: "images/be10X.pdf"
   },
   {
     title: "Cyber Security Webinar",
     description: "Ethical hacking fundamentals.",
-    image: "images/certificate2.png"
+    image: "images/certificate2.png",
+    pdf: "images/certificate2.pdf"
   }
 ];
