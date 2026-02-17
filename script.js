c7584ed23cbdc12c4462128bc97a74e21bf1cc6e
Deep Lambhade <deeplambhade101@gmail.com>
2026-02-17 07:43:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-
   const exploreBtn = document.getElementById("exploreBtn");
-
   const cardsContainer = document.getElementById("cardsContainer");
 
-
-
   exploreBtn.addEventListener("click", function () {
-
     if (!cardsContainer.classList.contains("show")) {
-
-      // clear previous cards
 
       cardsContainer.innerHTML = "";
 
-
-
       certifications.forEach(function (cert) {
-
         const card = document.createElement("div");
-
         card.classList.add("card");
 
-
-
         card.innerHTML = `
-
           <img src="${cert.image}" alt="${cert.title}">
-
           <h3>${cert.title}</h3>
-
           <p>${cert.description}</p>
-
           <button onclick="window.open('${cert.pdf}', '_blank')">View Certificate</button>
-
         `;
 
         cardsContainer.appendChild(card);
-
       });
-
-
 
       cardsContainer.classList.add("show");
 
       setTimeout(() => {
-
         cardsContainer.scrollIntoView({ behavior: "smooth" });
-
       }, 100);
-
     }
-
   });
-
 });
