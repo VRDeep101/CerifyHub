fbd3d67f9c1093ce604ff894478d1c8bdade6fe8
Deep Lambhade <deeplambhade101@gmail.com>
2026-02-17 15:33:37 +0530

    Okk

--- data.js
+++ data.js
@@ -1,46 +1,23 @@
 const certifications = [
-
   {
-
-    title: "Be10x AI Masterclass",
-
-    description: "Completed the Be10X AI Workshop, learned 15+ AI tools focused on productivity, automation, and workflow optimization.",
-
-    image: "images/be10X.jpg",
-
-    pdf: "images/be10X.pdf"
-
-  },
-
-  {
-
-    title: "Cyber Security",
-
-    description: "Completed a Cyber Security Workshop using Kali Linux – gained hands-on experience in basic ethical hacking, network analysis, and vulnerability assessment.",
-
-    image: "images/TUAcybersecuruty.jpg",
-
-    pdf: "images/TUAcybersecuruty.pdf"
-
-  },
-
-  {
-
-    title: "ITESA WebWorkshop",
-
-    description: " Led a Web Development Workshop for first-year students, teaching HTML, CSS, and JavaScript fundamentals, and helping them build their first structured and responsive web pages.",
-
+    title: "Itesa Web Workshop",
+    description: "Completed the Itesa Web Workshop, learned web fundamentals and workflow optimization.",
     image: "images/ItesaLogo.jpg",
-
     pdf: "images/ItesaWebWorkshop.pdf"
-
+  },
+  {
+    title: "TUA Cyber Security",
+    description: "Learned ethical hacking fundamentals and cybersecurity practices.",
+    image: "images/TUAcybersecuruty.jpg",
+    pdf: "images/TUAcybersecuruty.pdf"
+  },
+  {
+    title: "Be10X AI Masterclass",
+    description: "Completed the Be10X AI Workshop, learned 15+ AI tools focused on productivity, workflow, and automation.",
+    image: "images/be10x.jpg",
+    pdf: "images/be10X.pdf"
   }
-
 ];
-
-
-
-
 
 
 
